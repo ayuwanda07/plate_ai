28f85edaf38302fe7b6ec37a7c9aee4fec1b4c54
wanda07 <ayuwandaazalia@gmail.com>
2026-08-15 23:31:35 +0700

    feat: add client w/ ts and server

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+
+
+    <h1 className="text-3xl font-bold text-blue-500 text-center mt-10">
+      Halo PlatePal!
+    </h1>
+  )
+}
+
+export default App
